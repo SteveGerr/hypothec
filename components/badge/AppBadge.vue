159a9d6f48d8f201ef7b0419141e60b0c54ad1ab
i.shunyaev <i.shunyaev@fact.digital>
2023-10-01 18:21:16 +0400

    [feat] added badge

---
+++ components/badge/AppBadge.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="badge" :style="{ backgroundColor: bgColor || '#083E4C', color }">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  bgColor: String,
+  color: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.badge {
+  display: inline-block;
+  padding: 10px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%;
+  border-radius: 10px;
+  color: $white;
+}
+</style>
